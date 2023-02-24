f139e62a5d9f872d1550f289fde759a4e4201914
taj <tajuddin070796@gmail.com>
2023-02-24 10:32:04 +0000

    file1.sh added

---
+++ file1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "welcome to shell scripting"
+echo "what is the name of this shell script"
+$0
+echo "print your hostname"
+hostname
+echo "print the server disc space"
+df -h
+echo "ping if server is up or down"
+ping $(hostname)
